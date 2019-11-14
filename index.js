4b358997eb2e2ff12bc32fcd85b90307a1acee28
Tywunda Shivers <tywunda.shivers@gmail.com>
2019-11-14 01:41:25 -0800

    adding modified index.html index.js and styles.css

---
+++ index.js
@@ -0,0 +1,23 @@
+/*
+document.querySelector('.time-block') 
+var div=document.querySelector('.time-block')
+*/
+
+// function to call calendar events using local storage
+$(document).ready(function() {
+    function renderCalendarEvents() {
+
+    var inputObject = {
+        nine: localStorage.getItem("nine"),
+        ten: localStorage.getItem("ten"),
+        eleven: localStorage.getItem("eleven"),
+        noon: localStorage.getItem("noon"),
+        one: localStorage.getItem("one"),
+        two: localStorage.getItem("two"),
+        three: localStorage.getItem("three"),
+        four: localStorage.getItem("four"),
+        five: localStorage.getItem("five"),
+    }
+
+    var keys = 
+};
