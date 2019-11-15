01fc0d774bc97502feb56d53f19bd4bf0d9efed4
Tywunda Shivers <tywunda.shivers@gmail.com>
2019-11-15 12:31:58 -0800

    adding modified index.html index.js and styles.css

--- index.js
+++ index.js
@@ -8,16 +8,39 @@ $(document).ready(function() {
     function renderCalendarEvents() {
 
     var inputObject = {
-        nine: localStorage.getItem("nine"),
-        ten: localStorage.getItem("ten"),
-        eleven: localStorage.getItem("eleven"),
-        noon: localStorage.getItem("noon"),
-        one: localStorage.getItem("one"),
-        two: localStorage.getItem("two"),
-        three: localStorage.getItem("three"),
-        four: localStorage.getItem("four"),
-        five: localStorage.getItem("five"),
+        nineSave: localStorage.getItem("nineSave"),
+        tenSave: localStorage.getItem("tenSave"),
+        elevenSave: localStorage.getItem("elevenSave"),
+        noonSave: localStorage.getItem("noonSave"),
+        oneSave: localStorage.getItem("oneSave"),
+        twoSave: localStorage.getItem("twoSave"),
+        threeSave: localStorage.getItem("threeSave"),
+        fourSave: localStorage.getItem("fourSave"),
+        fiveSave: localStorage.getItem("fiveSave"),
     }
 
-    var keys = 
+    var keys = Object.keys(inputObject);
+        for (var i = 0; i < keys.length + 1; i++) {
+            var textarea = $("textarea").eq(i);
+            var key = keys[i];
+            var value = inputObject[key];
+            textarea.val(value);
+        }
 };
+
+renderCalendarEvents();
+
+});
+
+// *Complete the following* : 
+
+// *ADD* eventListener when user clicks save button 
+
+/* $(".saveBtn").on("click", function(event) {
+    event.preventDefault();
+
+    var txt = $(this).
+} */
+
+// *CREATE* if/else statements to color code each hour
+// example: var timeOfDay = (moment().format("HH")) 
